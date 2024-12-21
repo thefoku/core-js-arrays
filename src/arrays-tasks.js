850a22bb2baa648acca3934ad743fa9535c7d2fb
thefoku <leekengham@gmail.com>
2024-12-21 20:50:02 +0400

    feat: solve 37 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -380,8 +380,11 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array(len * 2)
+    .fill(0)
+    .map((_, i) => i)
+    .filter((e) => e % 2 !== 0);
 }
 
 /**
